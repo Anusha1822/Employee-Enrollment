4e3c7f8d7ca63f4b42eb0453f8e43f3003f83ad1
Anusha1822 <62939141+Anusha1822@users.noreply.github.com>
2022-01-04 17:22:23 +0530

    code
    code

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+
+const mysql = require("mysql");
+
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "password",
+  database: "employeesystem",
+});
+
+app.post("/create", (req, res) => {
+  console.log(req.body);
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
+
+  db.query(
+    "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+    [name, age, country, position, wage],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted"); //to end the request, sent it a message saying it worked
+      }
+    }
+  );
+});
+
+app.get("/employees", (req, res) => {
+  db.query("SELECT * from employees", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Your server is running on port 3001");
+});
